b37e897c251327e2fa5f1aa3d979b5206f39b716
Chelsea <96267228+chelseabee@users.noreply.github.com>
2022-02-27 09:37:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-let adj1 = document.getElementById("adj1");
-let nn1 = document.getElementById("nn1");
-let vb1 = document.getElementById("vb1");
-let avb1 = document.getElementById("avb1");
-let adj2 = document.getElementById("adj2");
-let nn2 = document.getElementById("nn2");
-let nn3 = document.getElementById("nn3");
-let adj3 = document.getElementById("adj3");
-let vb2 = document.getElementById("vb2");
-let avb2 = document.getElementById("avb2");
-let vb3= document.getElementById("vb3");
-let adj4 = document.getElementById("adj4");
 
 document.getElementById("submit-btn").addEventListener('click', madlib);
 
-let displayFinal = document.getElementById("display-finished-lib");
 
+let userInput = document.getElementsByTagName('input');
+function getUserInput(){
+for (let j=0; j < userInput.length; j++){
+console.log(userInput[j].value);
+
+}
+}
+
+
+let displayFinal = document.getElementById("display-finished-lib");
 function madlib(){ 
  let h3 = document.createElement('h3');
  h3.textContent = 'A Day At The Zoo!';
@@ -22,36 +19,53 @@ function madlib(){
  
  let p = document.createElement('p');  
  p.textContent = `Today I went to the zoo. I saw a(n)
- ${adj1.value} 
- ${nn1.value} jumping up and down in its tree.
- He ${vb1.value} ${avb1.value}
- through the large tunnel that led to its ${adj2.value}
- ${nn2.value}. I got some peanuts and passed
- them through the cage to a gigantic gray ${nn3.value}
+ ${userInput[0].value} 
+ ${userInput[1].value} jumping up and down in its tree.
+ He ${userInput[2].value} ${userInput[3].value}
+ through the large tunnel that led to its ${userInput[4].value}
+ ${userInput[4].value}. I got some peanuts and passed
+ them through the cage to a gigantic gray ${userInput[6].value}
  towering above my head. Feeding that animal made
- me hungry. I went to get a ${adj3.value} scoop
+ me hungry. I went to get a ${userInput[7].value} scoop
  of ice cream. It filled my stomach. Afterwards I had to
- ${vb2.value} ${avb2.value} to catch our bus.
- When I got home I ${vb3.value} my
- mom for a ${adj4.value} day at the zoo. ` ;
+ ${userInput[8].value} ${userInput[9].value} to catch our bus.
+ When I got home I ${userInput[10].value} my
+ mom for a ${userInput[11].value} day at the zoo. ` ;
 displayFinal.appendChild(p);
 
+let div = document.getElementById('formDiv')
+div.classList.remove('formDiv');
+div.classList.add('notActive');
+
+displayFinal.classList.remove('notActive');
+displayFinal.classList.add('active');
 }
 
-let nextBtn = document.querySelectorAll("next-btn");
 
-let allBtns = document.querySelector("button"). addEventListener('click', toggleInput);
-console.log(allBtns);
-
-function toggleInput (){
-for (let i = 0; i <allBtns.length; i++){
-
-    if (allBtns.className === 'active'){
-        allBtns.className = 'inactive';
-      } else {
-        allBtns.className = 'active';
-      }
+let activeListToggle = document.getElementsByTagName('li');    
+function showQuestions (){    
+for(let i = 0; i < activeListToggle.length-1; i++){
+    activeListToggle[i].lastElementChild.addEventListener('click', function nextQuestion(){
+        if(activeListToggle[i].className === 'active'){
+            activeListToggle[i].classList.remove('active');
+            activeListToggle[i].classList.add('notActive'); 
+            console.log( activeListToggle[1]);
+            getUserInput();
+            
+            activeListToggle[i+1].classList.remove('notActive');
+            activeListToggle[i+1].classList.add('active');
+            
+        }else if (activeListToggle[i].className === 'notActive'){
+            activeListToggle[i].classList.toggle('active');
+        }
+        
+    })
 
 }
-
 }
+
+showQuestions ();
+
+
+
+
